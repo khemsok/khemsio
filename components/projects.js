fb319335371c674e2eb8db2812b110727db5f8db
Khem Sok <khemsok97@gmail.com>
2020-05-04 10:17:31 -0400

    Delete unused import

--- components/projects.js
+++ components/projects.js
@@ -7,13 +7,6 @@ import { projects } from "../utils/projects";
 // MUI
 import Typography from "@material-ui/core/Typography";
 import LinearProgress from "@material-ui/core/LinearProgress";
-import Container from "@material-ui/core/Container";
-import Card from "@material-ui/core/Card";
-import CardHeader from "@material-ui/core/CardHeader";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import Fade from "@material-ui/core/Fade";
--- src/theme.js
+++ src/theme.js
@@ -1,9 +1,6 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 import { red } from "@material-ui/core/colors";
 
-// export const primaryColor = "#FF3E55"; //#2DC2BB #FF3E55
-// export const backgroundColor = "#fff";
-
 const typographyObj = {
   fontFamily:
     "Roboto, sans-serif, Roboto Mono, monospace, Roboto Condensed, sans-serif",
